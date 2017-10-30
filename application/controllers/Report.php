0d508bd04c01cc432fe9cfdb455202f9133d710a
wiwijuwita <wijuwita@gmail.com>
2017-10-31 03:10:13 +0700

    Reporting
    Export file Word dan PDF

--- application/controllers/Report.php
+++ application/controllers/Report.php
@@ -39,19 +39,43 @@ class Report extends CI_Controller
         $this->load->view('Pengajuan_doc',$data);
     }
 
-     function PDFDokumen()
+     function PDFDokumen($id_form = NULL)
     {
+        $this->load->model('Form_content_model');
+        $item_list = $this->Form_content_model->get_all_detail_by_form($id_form);
+        $this->load->model('Division_model');
+        $form_data = $this->Form_model->get_by_id($id_form);
+        $divisi = $this->Division_model->get_by_id($form_data->id_division);
+        $this->load->model('Tracking_model');
+        $form_acc = $this->Tracking_model->get_by_id_tracking_TU($form_data->id_tracking);
+
         $data = array(
-            'form_data' => $this->Form_model->get_all(),
-            'start' => 0
+            'divisi' => $divisi,
+            'form_data' => $form_data,
+            'item_list' => $item_list,
+            'form_acc' => $form_acc
         );
         
         ini_set('memory_limit', '32M');
+
+        //load the view and saved it into $html variabel
         $html = $this->load->view('Pengajuan_PDF', $data, true);
-        $this->load->library('pdf');
-        $pdf = $this->pdf->load();
-        $pdf->WriteHTML($html);
-        $pdf->Output('Lembar Pengajuan.pdf', 'D'); 
+
+        //this the PDF file name that user will get download
+        $pdfFilePath = "Lembar Pengajuan.pdf";
+
+        //load mPDF library
+        $this->load->library('m_pdf');
+
+        //generate the PDF from the given html
+        $this->m_pdf->pdf->WriteHTML($html);
+
+        //download it.
+        $this->m_pdf->pdf->Output($pdfFilePath, "D");
+
+        //$pdf = $this->pdf->load();
+        //$pdf->WriteHTML($html);
+        //$pdf->Output('Lembar Pengajuan.pdf', 'D'); 
     }
    
 }
