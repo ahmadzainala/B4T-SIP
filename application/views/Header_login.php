e4bedcf220d5ace68b26b60bd588a40d5eb45e9e
ranggirahman <ranggirahman@gmail.com>
2017-11-21 04:07:04 +0700

    panduan-added

--- application/views/Header_login.php
+++ application/views/Header_login.php
@@ -25,21 +25,26 @@
     <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
       <div class="container">
         <a class="navbar-brand" href="<?php echo base_url(); ?>Main"><img src="<?php echo base_url() ?>template/user/img/logo.png" class="img-fluid" style="max-width: 20%; and height: auto" alt="Responsive image"> Balai Besar Bahan dan Barang Teknik</a>
-        <a href="" class="btn btn-warning" role="button"><i class="material-icons" style="font-size: 22px">book</i> Bantuan</a>
-        <div class="dropdown">
-          <button class="btn btn-info dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="material-icons" style="font-size: 22px">account_circle</i><?php echo " ".$this->session->userdata('username'); ?></button>
-          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownMenuButton">
-            <table>
-              <tr>
-                <td rowspan="2">
-                   <img src="<?php echo base_url() ?>uploads/profile/<?php echo $this->session->id_user;?>.jpg?dummy=8484744" class="rounded" height="50px" width="50px" onerror=this.src="<?php echo base_url() ?>template/user/img/default_profile.jpg" style="margin: 0px 0px 0px 20px;">
-                </td>
-                <td>
-                  <a class="dropdown-item" href="<?php echo base_url() ?>User_akun/edit_profile">Edit Profile</a>
-                  <a class="dropdown-item" style="color: red;" href="<?php echo base_url() ?>Login/logout">Logout</a>
-                </td>
-              </tr>
-            </table>           
+        <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+          <a href="" class="btn btn-warning" role="button"><i class="material-icons" style="font-size: 22px">book</i> Panduan</a>
+
+          <div class="btn-group" role="group">
+            <button id="btnGroupDrop1" type="button" class="btn btn-info dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+               <i class="material-icons" style="font-size: 22px">account_circle</i><?php echo " ".$this->session->userdata('username'); ?>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+               <table>
+                  <tr>
+                    <td rowspan="2">
+                       <img src="<?php echo base_url() ?>uploads/profile/<?php echo $this->session->id_user;?>.jpg?dummy=8484744" class="rounded" height="50px" width="50px" onerror=this.src="<?php echo base_url() ?>template/user/img/default_profile.jpg" style="margin: 0px 0px 0px 20px;">
+                    </td>
+                    <td>
+                      <a class="dropdown-item" href="<?php echo base_url() ?>User_akun/edit_profile">Edit Profile</a>
+                      <a class="dropdown-item" style="color: red;" href="<?php echo base_url() ?>Login/logout">Logout</a>
+                    </td>
+                  </tr>
+                </table>           
+            </div>
           </div>
         </div>       
       </div>
